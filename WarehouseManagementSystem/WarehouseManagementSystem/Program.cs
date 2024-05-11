370faa15c6b40d222d61bdae62aa5d4b2c55fe13
EternalWanderer <eternal.wanderer.u@gmail.com>
2024-05-11 10:04:52 +0700

    Initial commit

---
+++ WarehouseManagementSystem/WarehouseManagementSystem/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using log4net.Config;
+using System.Reflection;
+using System.IO;
+using System.Security.Principal;
+
+namespace WarehouseManagementSystem
+{
+    internal class Program
+    {
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        static void Main(string[] args)
+        {
+            // Инициализация log4net из файла конфигурации
+            XmlConfigurator.Configure(new FileInfo("log4net.config"));
+
+            // Пример использования логгирования
+            log.Info("Программа управления складом запущена.");
+
+            Warehouse warehouse = new Warehouse();
+
+            // Добавление товаров на склад
+            Product product1 = new Product { Name = "Лампа", Code = "001", Price = 10.99m, Quantity = 50 };
+            Product product2 = new Product { Name = "Стол", Code = "002", Price = 150.99m, Quantity = 20 };
+
+            warehouse.AddProduct(product1);
+            warehouse.AddProduct(product2);
+
+            // Вывод списка товаров на складе
+            warehouse.DisplayProducts();
+
+            // Удаление товара со склада
+            warehouse.RemoveProduct(product1);
+
+            // Вывод обновленного списка товаров на складе
+            warehouse.DisplayProducts();
+
+            log.Info("Программа управления складом завершена.");
+
+            Console.ReadLine(); // Ожидание нажатия клавиши для завершения программы
+        }
+    }
+}
---
+++ WarehouseManagementSystem/WarehouseManagementSystem/Warehouse.cs
@@ -0,0 +1,57 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseManagementSystem
+{
+    public class Warehouse
+    {
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        private List<Product> products;
+
+        public Warehouse()
+        {
+            products = new List<Product>();
+        }
+
+        public void AddProduct(Product product)
+        {
+            products.Add(product);
+            log.Info($"Товар \"{product.Name}\" добавлен на склад.");
+        }
+
+        public void RemoveProduct(Product product)
+        {
+            if (products.Remove(product))
+            {
+                log.Info($"Товар \"{product.Name}\" удален со склада.");
+            }
+            else
+            {
+                log.Warn($"Товар \"{product.Name}\" не найден на складе.");
+            }
+        }
+
+        public void DisplayProducts()
+        {
+            log.Info("Список товаров на складе:");
+            foreach (var product in products)
+            {
+                log.Info($"- {product.Name} (Код: {product.Code}, Цена: {product.Price}, Количество: {product.Quantity})");
+            }
+        }
+    }
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
+
