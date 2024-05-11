d31ba0fe7e37c5ec2a715b10472e173a1e7a8e0d
EternalWanderer <eternal.wanderer.u@gmail.com>
2024-05-11 10:05:58 +0700

    Add welcome message

---
+++ Test/Test/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyLibrary;
+
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+
+            int result = calc.Add(5, 3);
+            Console.WriteLine("5 + 3 = " + result);
+
+            int result2 = calc.Subtract(10, 4);
+            Console.WriteLine("10 - 4 = " + result2);
+
+            Console.ReadKey();
+        }
+    }
+}
